84b084a7f496563de299fcf9166c6cbf50d2264d
qrNasr <83606441+qrNasr@users.noreply.github.com>
2021-06-03 22:09:17 +0300

    Create or.vb

---
+++ or.vb
@@ -0,0 +1,16 @@
+Imports System
+				
+Public Module Module1
+	Public Sub Main()
+		dim A as integer =12
+		dim B as integer = 13
+		
+		if A or B > 12 
+			console.WriteLine("Bingo")
+		else
+			
+			console.WriteLine("sorry")
+			
+		end if
+	End Sub
+End Module
